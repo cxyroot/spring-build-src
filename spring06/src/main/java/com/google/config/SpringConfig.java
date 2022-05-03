88f6970c0428dd12c60fd9f4c5572aa9377aabfa
CAOXIAOYANG <1076675153@qq.com>
2022-05-03 23:51:39 +0800

    add ComponentScan

--- spring06/src/main/java/com/google/config/SpringConfig.java
+++ spring06/src/main/java/com/google/config/SpringConfig.java
@@ -5,6 +5,7 @@ import com.google.service.UserService;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 /**
  * 注解驱动开发方式
@@ -15,9 +16,18 @@ import org.springframework.stereotype.Component;
 //@ComponentScan(basePackages = "com.google")
 //@ComponentScan(basePackageClasses = UserService.class)
 //@ComponentScan
-@ComponentScan(basePackages = "com.google",nameGenerator = MyBeanNameGenerator.class,resourcePattern = "**/*.class" )//BeanNameGenerator
+@ComponentScan(basePackages = "com.google",
+	nameGenerator = MyBeanNameGenerator.class,
+	resourcePattern = "**/*.class",
+	// includeFilters = @ComponentScan.Filter(value = Service.class),
+	// excludeFilters = @ComponentScan.Filter(value = Service.class),
+	excludeFilters = {},
+	lazyInit = false
+)//BeanNameGenerator
 //ScopeMetadataResolver
 //resourcePattern String resourcePattern() default ClassPathScanningCandidateComponentProvider.DEFAULT_RESOURCE_PATTERN;
+
+//excludeFilters org.springframework.context.annotation.FilterType
 public class SpringConfig {
 
 }
